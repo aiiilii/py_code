aa38011c3ac3b742396300b1ec66947c83eb93db
Aili Gong <aili.hgong@gmail.com>
2020-07-07 20:16:37 +0000

    Added lru_cache

---
+++ lru_cache.py
@@ -0,0 +1,38 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.cache = {}
+        self.next = {}
+        self.before = {}
+        self.head, self.tail = '#', '$'
+        self.connect(self.head, self.tail)
+
+    def connect(self, a, b):
+        self.next[a], self.before[b] = b, a
+
+    def delete(self, key):
+        self.connect(self.before[key], self.next[key])
+        del self.before[key], self.next[key], self.cache[key]
+
+    def append(self, key, val):
+        self.cache[key] = val
+        self.connect(self.before[self.tail], key) # append to the end
+        self.connect(key, self.tail)
+        if len(self.cache) > self.size:
+            self.delete(self.next[self.head]) # delete the lru at the front
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        val = self.cache[key]
+        self.delete(key)
+        self.append(key, val)
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.delete(key)
+
+        self.append(key, value)
