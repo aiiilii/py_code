f7be97478f4ccd8e01f5762fef3a4065783e1bbf
Aili Gong <aili.hgong@gmail.com>
2020-07-08 04:05:03 +0000

    Added notes to lur_cache

--- lru_cache.py
+++ lru_cache.py
@@ -13,7 +13,7 @@ class LRUCache:
 
     def delete(self, key):
         self.connect(self.before[key], self.next[key])
-        del self.before[key], self.next[key], self.cache[key]
+        del self.before[key], self.next[key], self.cache[key] # delete the before pointer to key and next pointer to key by deleting the key-value pair in before dict and next dict;
 
     def append(self, key, val):
         self.cache[key] = val
@@ -26,7 +26,7 @@ class LRUCache:
         if key not in self.cache:
             return -1
 
-        val = self.cache[key]
+        val = self.cache[key] 
         self.delete(key)
         self.append(key, val)
         return val
