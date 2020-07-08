380002573f46afb9a85f7d6f468640ea3f358d77
Aili Gong <aili.hgong@gmail.com>
2020-07-08 22:30:55 +0000

    Added longest_palindromic_substring

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,24 @@
+class LongestPalindrome:
+
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            temp = self.helper(s, i, i)
+            if len(temp) > len(res):
+                res = temp
+            
+            # even case, like "abba"
+            temp = self.helper(s, i, i + 1)
+            if len(temp) > len(res):
+                res = temp
+
+        return res
+
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s: str, l: int, r: int) -> str:
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1 # move leftward from the left
+            r += 1 #move rightward from the right
+        return s[l+1:r]
