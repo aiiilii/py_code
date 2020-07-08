574f0560e496f441dcc89ca07a094bb8e900b689
Aili Gong <aili.hgong@gmail.com>
2020-07-08 22:52:39 +0000

    Added merge_k_sorted_lists

---
+++ merge_k_sorted_lists.py
@@ -0,0 +1,29 @@
+import queue
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MergeKSortedLists:
+
+    # using pq
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dummy = ListNode()
+        l3 = dummy
+        pq = queue.PriorityQueue()
+
+        for idx, node in enumerate(lists):
+            if node:
+                pq.put((node.val, idx, node)) # double parens are needed
+
+        while not pq.empty():
+            _, idx, node = pq.get()
+        
+            l3.next = node
+            l3 = l3.next
+            
+            if l3.next:
+                pq.put((node.next.val, idx, node.next))
+
+        return dummy.next
