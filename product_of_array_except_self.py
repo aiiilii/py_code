a77942a56b4b657572c49f82f04a594362ce15cf
Aili Gong <aili.hgong@gmail.com>
2020-07-09 21:01:11 +0000

    Added product_of_array_except_self

---
+++ product_of_array_except_self.py
@@ -0,0 +1,17 @@
+class ProductExceptSelf:
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+
+        res = [0] * length
+
+        res[0] = 1
+        for i in range(1, length):
+            res[i] = nums[i - 1] * res[i - 1]
+
+        R = 1
+        for i in reversed(range(length)):
+            res[i] = res[i] * R
+            R *= nums[i]
+
+        return res
