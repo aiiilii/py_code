b97b1f0a5e6730ad847a944782c125e4b7ccb73a
Aili Gong <aili.hgong@gmail.com>
2020-07-10 21:37:49 +0000

    Added best_time_to_buy_and_sell_stock

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+class BestTimeToBuyAndSellStock:
+
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        max_profit = 0
+        min_price = float('inf') # for max
+
+        for i in range(len(prices)):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
+
+        return max_profit
