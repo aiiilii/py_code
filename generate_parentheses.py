47348e9faa79a2b6815e3b27b043914b2aeae64e
Aili Gong <aili.hgong@gmail.com>
2020-07-11 18:20:29 +0000

    Added generate_parenthese

---
+++ generate_parentheses.py
@@ -0,0 +1,23 @@
+class GenerateParentheses:
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        if n == 0:
+            return res
+
+        templist = ''
+        self.backtracking(res, templist, 0, 0, n)
+
+        return res
+
+    def backtracking(self, res: List[str], templist: str, left: int, right: int, max_paren: int) -> None:
+        if len(templist) == max_paren * 2:
+            res.append(templist) # same as (res += [templist])
+            return
+
+        if left < max_paren:
+            self.backtracking(res, templist + '(', left + 1, right, max_paren)
+
+        if right < left:
+            self.backtracking(res, templist + ')', left, right + 1, max_paren)
