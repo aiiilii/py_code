ff99e5e6330f8802f5672cff923ed6ff65d7368b
Aili Gong <aili.hgong@gmail.com>
2020-07-15 09:46:19 -0700

    Added meeting_rooms_ii

---
+++ meeting_rooms_ii.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class MeetingRoomsII:
+
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if len(intervals) == 0 or len(intervals[0]) != 2:
+            return 0
+
+        start = []
+        end = []
+
+        for i in range(len(intervals)):
+            start.append([intervals[i][0]])
+            end.append([intervals[i][1]])
+
+        start.sort()
+        end.sort()
+
+        res = 0
+        end_point = 0
+
+        for i in range(len(start)):
+            if start[i] < end[end_point]:
+                res += 1
+            else:
+                end_point += 1
+
+        return res
