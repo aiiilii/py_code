760825a6ff28e14f6ab565b46c06d105d72b41d6
Aili Gong <aili.hgong@gmail.com>
2020-07-18 16:49:47 -0700

    Added permutations

---
+++ permutations.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Permutation:
+
+    def permute(self, nums: List[int]) -> List[int]:
+        
+        def backtrack(first=0):
+            # if all integers are used up
+            if first == n:
+                res.append(nums[:])
+
+            for i in range(first, n):
+                # place i-th integer first in the current permutation
+                # swap
+                nums[first], nums[i] = nums[i], nums[first]
+                # use next integers to complete the permutations
+                backtrack(first + 1)
+                # backtrack (swap back)
+                nums[first], nums[i] = nums[i], nums[first]
+
+        n = len(nums)
+        res = []
+        backtrack()
+        return res
