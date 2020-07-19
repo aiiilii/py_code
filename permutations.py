abe35f88301985179ddbdb5cd4a8336f2da02d74
Aili Gong <aili.hgong@gmail.com>
2020-07-18 17:06:18 -0700

    Added another solution to permutations

--- permutations.py
+++ permutations.py
@@ -22,3 +22,22 @@ class Permutation:
         res = []
         backtrack()
         return res
+
+
+    def permute1(self, nums: List[int]) -> List[List[int]]:
+        
+        # can also pass the nums in as value not reference
+        def backtrack(nums, first=0):
+            if first == n:
+                res.append(list(nums))
+                return
+
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(list(nums), first + 1)
+                nums[first], nums[i] = nums[i], nums[first]
+
+        n = len(nums)
+        res = []
+        backtrack(nums)
+        return res
