53456eeb55c8bb14fef4f34d27accffeff58cda9
Aili Gong <aili.hgong@gmail.com>
2020-07-21 16:30:40 -0700

    small change to decode_string

--- decode_string.py
+++ decode_string.py
@@ -1,5 +1,35 @@
 class DecodeString:
 
+    # using 1 stack
+    def decodeString(self, s: str) -> str:
+        if not str:
+            return ""
+
+        stack = []
+        curr = ""
+        k = 0
+
+        for ch in s:
+            if ch.isdigit():
+                k = k * 10 + int(ch)
+            elif ch == '[':
+                stack.append(k)
+                stack.append(curr)
+                k = 0
+                curr = ""
+            elif ch == ']':
+                temp = curr
+                curr = stack.pop()
+
+                for i in range(stack.pop(), 0, -1):
+                    curr += temp
+            else:
+                curr += ch
+
+        return curr
+
+
+    # using two stacks
     def decodeString(self, s: str) -> str:
         if not str:
             return ""
